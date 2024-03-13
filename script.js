5d5a4bf364a444833c817000ac3cafa67cbf7983
PRRInc <prrincdev@gmail.com>
2024-03-12 20:16:18 -0400

    Customized Styling

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = event.target.title.value;
+  const author = event.target.author.value;
+
+  const ul = document.querySelector("ul");
+  const li = document.createElement("li");
+
+  li.innerHTML = `<span>${checkedGenre}</span>${title} by ${author}
+  <button>Finished</button>`;
+
+  const bookButton = li.querySelector("button");
+  removeBook(li);
+  ul.append(li);
+
+  form.reset();
+});
